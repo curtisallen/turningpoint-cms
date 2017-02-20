1b1222f33ac88cd73f108ccddef421aa66c313cb
Curtis Allen <curtis.allen@robotsandpencils.com>
2017-02-20 13:51:11 -0700

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,7 @@
 var gulp = require('gulp');
-var watch = require('gulp-watch');
-var shell = require('gulp-shell')
+var shell = require('gulp-shell');
 
-const dbName = 'tp-mongo'
+const dbName = 'tp-mongo';
 
 var paths = {
 	'src':['./models/**/*.js','./routes/**/*.js', 'keystone.js', 'package.json']
@@ -10,12 +9,9 @@ var paths = {
 };
 
 gulp.task('runKeystone', shell.task('node keystone.js'));
-gulp.task('watch', [
-
-]);
 
 gulp.task('createMongo', shell.task(`docker run --name ${dbName} -p 27017:27017 -d mongo`));
-gulp.task('startMongo', shell.task(`docker start ${dbName}`))
+gulp.task('startMongo', shell.task(`docker start ${dbName}`));
 gulp.task('stopMongo', shell.task(`docker stop ${dbName}`));
 
-gulp.task('default', ['watch', 'runKeystone']);
+gulp.task('default', ['runKeystone']);
