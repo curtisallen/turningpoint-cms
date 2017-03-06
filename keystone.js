fe7339c53537b4c30c22c35831a47689733afc2a
Curtis Allen <curtis.allen@robotsandpencils.com>
2017-03-05 19:14:02 -0700

    mongo session store

--- keystone.js
+++ keystone.js
@@ -23,7 +23,19 @@ keystone.init({
 
 	'auto update': true,
 	'session': true,
-	//'session store': 'mongo',
+	'session store': 'connect-mongostore',
+	"sessionStore": {
+  "db": {
+    "name": "turningpoint",
+    "username": process.env.MONGO_USER,
+    "password": process.env.MONGO_PASSWORD,
+    "servers": [
+      { "host": "ds159079-a0.mlab.com", "port": 59079 },
+      { "host": "ds159079-a1.mlab.com", "port": 59079 },
+      { "host": "ds159079-ar0.mlab.com", "port": 59079 }
+    ]
+  }
+	}
 	'auth': true,
 	'user model': 'User',
 	'signin redirect': '/',
